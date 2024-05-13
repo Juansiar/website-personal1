1290ac56e6d54d7d1bbb6b2fdfaf9c951d5ae5a7
Juansiar <christiansiar14@gmail.com>
2024-05-13 12:07:30 +0800

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,52 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+/**
+ * Requires the "PHP Email Form" library
+ * The "PHP Email Form" library is available only in the pro version of the template
+ * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+ * For more info and help: https://bootstrapmade.com/php-email-form/
+ */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'christiansiar14@gmail.com';
+// Configuration
+$receiving_email_address = 'www.christiansiar14@gmail.com';
+$php_email_form_path = '../assets/vendor/php-email-form/php-email-form.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Load the PHP Email Form library
+if (!file_exists($php_email_form_path)) {
+    throw new Exception('Unable to load the "PHP Email Form" Library!');
+}
+require_once $php_email_form_path;
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Create a new PHP Email Form instance
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+// Set the recipient email address
+$contact->to = $receiving_email_address;
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Validate and set the sender information
+$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+$subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
 
-  echo $contact->send();
-?>
+if (!$name ||!$email ||!$subject) {
+    throw new Exception('Invalid input data');
+}
+
+$contact->from_name = $name;
+$contact->from_email = $email;
+$contact->subject = $subject;
+
+// Add message fields
+$contact->add_message($name, 'From');
+$contact->add_message($email, 'Email');
+$contact->add_message($_POST['message'], 'Message', 10);
+
+// Send the email
+try {
+    $result = $contact->send();
+    echo $result;
+} catch (Exception $e) {
+    // Log the error or display a user-friendly error message
+    error_log($e->getMessage());
+    echo 'Error sending email. Please try again later.';
+}
